18bd802588bfe52b6c3c29f9b8bf65c7c54e7401
djhest <jdemuth18@gmail.com>
2026-03-09 21:59:14 +0100

    chore: added should i buy project

--- app/projects/data.ts
+++ app/projects/data.ts
@@ -4,7 +4,7 @@ export type Project = {
   title: string;
   description: string;
   url: string;
-  type: "Tool" | "Experiment" | "Side project" | "Open source" | "Template";
+  type: "Tool" | "Experiment" | "Side project" | "Open source" | "Template" | "Personal project";
   tech?: Tech[];
 };
 
@@ -19,6 +19,13 @@ export const projects: Project[] = [
   //   url: "https://example.com",
   //   type: "tool",
   // },
+  {
+    title: "Should i buy this vinyl?",
+    description: "A LLM based app for vinyl newbies to help them decide if they should buy a vinyl record.",
+    url: "https://shouldibuythisvinyl.com",
+    type: "Personal project",
+    tech: ["nextjs", "typescript", "tailwind", "vercel", "openai"],
+  },
   {
     title: "Slaatto Morsbøl",
     description: "Website made for Slaatto Morsbøl, a Copenhagen based architecture studio.",
